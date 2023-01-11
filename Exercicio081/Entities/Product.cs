b44fcb2b24699f5fb550be8df6784698a34c1e92
Guilherme Pascoal <gui_pascoalcruz@hotmail.com>
2023-01-11 13:03:58 -0300

    Criada aplicação para coletar pedido do cliente

---
+++ Exercicio081/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercicio081.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Exercicio081/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Globalization;
+using Exercicio081.Entities;
+using Exercicio081.Entities.Enums;
+
+namespace Exercicio081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth Date (MM/DD/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus status = (OrderStatus)Enum.Parse(typeof(OrderStatus), Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double priceProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(nameProduct, priceProduct);
+
+                Console.Write("Quantity: ");
+                int quantityProduct = int.Parse(Console.ReadLine());
+                
+                OrderItem item = new OrderItem(quantityProduct, priceProduct, product);
+                order.AddItem(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
